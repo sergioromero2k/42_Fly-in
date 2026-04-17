f657467549631e71640cc6af965fec96ca105c80
Sergio Alejandro <sergioromero2k@gmail.com>
2026-04-17 12:23:37 +0200

    Day 02: Add PathFinder with Dijkstra algorithm

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
 
 from parser.map_parser import Parser
+from pathfinder.pathfinder import PathFinder
 
 
 def main() -> None:
-    parser = Parser()
-    graph_1 = parser.parse("maps/easy/01_linear_path.txt")
-    graph_2 = parser.parse("maps/easy/02_simple_fork.txt")
-    graph_3 = parser.parse("maps/easy/03_basic_capacity.txt")
-    graph_4 = parser.parse("maps/hard/01_maze_nightmare.txt")
+    # parser = Parser()
+    # graph_1 = parser.parse("maps/easy/01_linear_path.txt")
+    # graph_2 = parser.parse("maps/easy/02_simple_fork.txt")
+    # graph_3 = parser.parse("maps/easy/03_basic_capacity.txt")
+    # graph_4 = parser.parse("maps/hard/01_maze_nightmare.txt")
 
-    print(graph_1)
-    print(graph_2)
-    print(graph_3)
-    print(graph_4)
+    # print(graph_1)
+    # print(graph_2)
+    # print(graph_3)
+    # print(graph_4)
+
+    parser = Parser()
+    graph = parser.parse("maps/medium/03_priority_puzzle.txt")
+
+    pf = PathFinder(graph)
+    path = pf.find_path(graph.start, graph.end)
+
+    for zone in path:
+        print(zone.name)
 
 
 if __name__ == "__main__":
--- pathfinder/pathfinder.py
+++ pathfinder/pathfinder.py
@@ -1,5 +1,68 @@
 #!/usr/bin/env python3
 
+from models.zone import Zone
+from models.graph import Graph
+from typing import List
+import heapq
+
 
 class PathFinder:
-    ...
+    def __init__(self, graph: Graph) -> None:
+        self.graph = graph
+
+    def get_neighbors(self, zone: Zone) -> List[Zone]:
+        neighbors = []
+        for connection in self.graph.connections:
+            if connection.zone_a == zone:
+                neighbor = connection.zone_b
+                if neighbor.zone_type != "blocked":
+                    neighbors.append(connection.zone_b)
+            elif connection.zone_b == zone:
+                neighbor = connection.zone_a
+                if neighbor.zone_type != "blocked":
+                    neighbors.append(connection.zone_a)
+        return neighbors
+
+    def get_cost(self, zone: Zone) -> float:
+        if zone.zone_type == "normal":
+            return 1
+        if zone.zone_type == "priority":
+            return 1
+        if zone.zone_type == "restricted":
+            return 2
+        if zone.zone_type == "blocked":
+            return float("inf")
+        return 1
+
+    def find_path(self, start: Zone, end: Zone) -> List[Zone]:
+        distances = {zone: float("inf") for zone in self.graph.zones}
+        distances[start] = 0
+
+        previous: dict[Zone, Zone] = {}
+        visited = set()
+
+        counter = 0
+        queue: list[tuple[float, int, Zone]] = [(0, counter, start)]
+        while queue:
+            cost, _, zone = heapq.heappop(queue)
+            if zone == end:
+                path = []
+                current_zone = end
+                while current_zone in previous:
+                    path.append(current_zone)
+                    current_zone = previous[current_zone]
+                path.append(start)
+                path.reverse()
+                return path
+
+            if zone in visited:
+                continue
+            visited.add(zone)
+            for neighbor in self.get_neighbors(zone):
+                new_cost = cost + self.get_cost(neighbor)
+                if new_cost < distances[neighbor]:
+                    distances[neighbor] = new_cost
+                    previous[neighbor] = zone
+                    counter += 1
+                    heapq.heappush(queue, (new_cost, counter, neighbor))
+        return []
